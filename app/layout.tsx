27176372e643ed214f2f1df9e07b66b050a0551a
kosovic-creator <drasko.kosovic@icloud.com>
2026-01-04 07:13:32 +0100

    asasas

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    
+      <head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        {/* ostali meta tagovi */}
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider>
           <div className="flex">
