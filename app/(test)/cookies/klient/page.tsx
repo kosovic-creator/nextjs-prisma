5073a6be66b492b7c2874992b674f062c4855aa5
kosovic-creator <drasko.kosovic@icloud.com>
2026-01-06 07:07:36 +0100

    cookies 2

--- app/(test)/cookies/klient/page.tsx
+++ app/(test)/cookies/klient/page.tsx
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
 import { useEffect } from 'react'
 import { setThemeCookie } from '@/actions/cookies'
 
 export default function ThemeSetter() {
-  useEffect(() => {
-    setThemeCookie('dark')  // Ili new Date().toISOString()
-  }, [])
+  // useEffect(() => {
+  //   setThemeCookie('dark')  // Ili new Date().toISOString()
+  // }, [])
 
-  return <button onClick={() => setThemeCookie('light')}>Postavi theme</button>
+  return <>
+    <div>Postavi theme cookie:</div>
+    <div className='flex flex-col gap-4'>
+      <button onClick={() => setThemeCookie('light')}>Postavi theme light</button>
+      <button onClick={() => setThemeCookie('dark')}>Postavi theme dark</button>
+    </div>
+  </>
+
+
+
 }
