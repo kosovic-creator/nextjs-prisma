b071ac1853827c61298e282170c497940e4eec76
kosovic-creator <drasko.kosovic@icloud.com>
2026-01-22 23:18:33 +0100

    sas

--- actions/test.ts
+++ actions/test.ts
@@ -1,18 +1,13 @@
 import { redirect } from 'next/navigation';
 import prisma from "@/lib/prisma";
-import { request } from "https";
 
 export async function testAction() {
-  // Dummy fetch
-
-  await fetch('https://example.com', { cache: 'no-store' });
+  await fetch('https://example.com', { cache: 'no-store' }); // workaround
 
   const test_data = await prisma.testModel.findMany();
   return test_data;
 }
 export async function addTestData(formData: FormData): Promise<void> {
-  // Pristupi ne-keširanim podacima
-  await fetch('https://example.com', { cache: 'no-store' });
 
   await prisma.testModel.create({
     data: {
@@ -21,5 +16,5 @@ export async function addTestData(formData: FormData): Promise<void> {
     },
   });
 
-  redirect('/test-data'); // Ovdje radiš redirect
+  redirect('/test-data');
 }
